a1a934adc898a12d74d56518b885ca3ba6bffb8a
Portablejim <portablejim@jamesmckay.id.au>
2024-10-14 01:40:54 +1100

    Update schema to get page displaying

--- src/app/_graphql/pages.ts
+++ src/app/_graphql/pages.ts
@@ -1,6 +1,4 @@
-import { ARCHIVE_BLOCK, CALL_TO_ACTION, CONTENT, MEDIA_BLOCK } from './blocks'
-import { LINK_FIELDS } from './link'
-import { MEDIA } from './media'
+import { CALL_TO_ACTION, CONTENT, MEDIA_BLOCK } from './blocks'
 import { META } from './meta'
 
 export const PAGES = `
@@ -19,20 +17,11 @@ export const PAGE = `
       docs {
         id
         title
-        hero {
-          type
-          richText
-          links {
-            link ${LINK_FIELDS()}
-          }
-          ${MEDIA}
-        }
         layout {
           ${CONTENT}
           ${CALL_TO_ACTION}
           ${CONTENT}
           ${MEDIA_BLOCK}
-          ${ARCHIVE_BLOCK}
         }
         ${META}
       }
