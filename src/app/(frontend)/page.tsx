abdab2ca9115e31f368b444d2d73fbc69419f5c4
James McKay <james.mckay@trausteknik.com>
2024-11-02 00:32:36 +1100

    Try to improve way page is rendered

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -5,3 +5,7 @@ export const dynamic = 'force-dynamic'
 export default PageTemplate
 
 export { generateMetadata }
+
+export function generateStaticParams() {
+  return [{ slug: 'home' }]
+}
--- src/app/(frontend)/series/[slug]/page.tsx
+++ src/app/(frontend)/series/[slug]/page.tsx
@@ -14,14 +14,6 @@ import { SeriesShow } from '../../../../components/SeriesShow'
 import { generateSeriesMeta } from '@/utilities/generateMeta'
 import notFound from '../../not-found'
 
-// Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
-// This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
-// To do this, we include the `no-cache` header on the fetch requests used to get the data for this page
-// But we also need to force Next.js to dynamically render this page on each request for preview mode to work
-// See https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
-// If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
-export const dynamic = 'force-dynamic'
-
 type Args = {
   params: Promise<{
     slug?: string
