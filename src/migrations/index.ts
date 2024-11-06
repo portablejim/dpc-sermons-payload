3b8fa89abf2308fdce7a8c04bd318c19632abf14
Portablejim <portablejim@jamesmckay.id.au>
2024-11-06 21:20:17 +1100

    Add codeblock migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241101_044723_initialDB from './20241101_044723_initialDB';
 import * as migration_20241106_004615_sermonlisttype from './20241106_004615_sermonlisttype';
+import * as migration_20241106_101919_codeblock from './20241106_101919_codeblock';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241106_004615_sermonlisttype.up,
     down: migration_20241106_004615_sermonlisttype.down,
-    name: '20241106_004615_sermonlisttype'
+    name: '20241106_004615_sermonlisttype',
+  },
+  {
+    up: migration_20241106_101919_codeblock.up,
+    down: migration_20241106_101919_codeblock.down,
+    name: '20241106_101919_codeblock'
   },
 ];
