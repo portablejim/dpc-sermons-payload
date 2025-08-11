362620e20142db3ac1d8d37cf254cc29d154962d
Portablejim <portablejim@jamesmckay.id.au>
2025-08-11 13:39:10 +1000

    Fix back link fill colour

--- src/app/(hub)/hub/[slug]/page.tsx
+++ src/app/(hub)/hub/[slug]/page.tsx
@@ -58,7 +58,7 @@ export default async function Page({ params: paramsPromise }: Args) {
   if(slug !== "members-hub") {
     backLink = <div className="container">
       <Link href={'./'} className={'pb-4 mb-2 inline-block capitalize'}>
-        <svg className={"backlinkIcon icon w-5 inline-block align-middle pr-1"} aria-label={"Back"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-providedby="Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.">
+        <svg className={"backlinkIcon icon w-5 dark:fill-white inline-block align-middle pr-1"} aria-label={"Back"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-providedby="Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.">
           <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
         Members Hub
       </Link>
