a0993a1be1a8296d6b480c5eb957df705b0d7d72
Portablejim <portablejim@jamesmckay.id.au>
2025-08-11 14:24:10 +1000

    Add some middleware to improve split domain

---
+++ src/middleware.ts
@@ -0,0 +1,53 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+function extractSubdomain(request: NextRequest): string | null {
+  const url = request.url;
+  const host = request.headers.get('host') || '';
+
+  if(host.startsWith('hub'))
+  {
+    return 'hub'
+  }
+
+  if(host.startsWith('talks'))
+  {
+    return 'talks'
+  }
+
+  return null;
+}
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const subdomain = extractSubdomain(request);
+
+  if (subdomain) {
+    console.log('sub domain')
+    // Block access to admin page from subdomains
+    if (pathname.startsWith('/admin')) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    // For the root path on a subdomain, rewrite to the subdomain page
+    if (pathname === '/') {
+      console.log({rewrite: new URL(`/${subdomain}`, request.url) })
+      return NextResponse.rewrite(new URL(`/${subdomain}`, request.url));
+    }
+  }
+
+  // On the root domain, allow normal access
+  console.log('root domain')
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api|_next|upload|[\\w-]+\\.\\w+).*)'
+  ]
+};
