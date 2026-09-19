68e3431268fd32444857052605467377ea0f2ed0
Portablejim <portablejim@jamesmckay.id.au>
2026-09-19 10:17:25 +1000

    Updating payload: Convert middleware to proxy

--- talks/src/proxy.ts
+++ talks/src/proxy.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   // On the root domain, allow normal access
   return NextResponse.next();
 }
